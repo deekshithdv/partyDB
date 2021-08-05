5c3366df18706d7e346dc22bcaf74f2855835fb7
Deekshith D V <deekshithdv563.com>
2021-08-05 19:38:32 +0530

    init

---
+++ 1_SQL/Query/20210805_table_creation.sql
@@ -0,0 +1,36 @@
+--t_placeテーブルの作るクイリ
+CREATE TABLE t_place(
+	place_id bigserial PRIMARY KEY, 
+	name text not null, 
+	address text not null
+);
+
+--ｔ_partyテーブルの作るクイリ
+CREATE TABLE t_party (
+	party_id bigserial PRIMARY KEY, 
+	title text not null, 
+	start_ts timestamptz not null, 
+	end_ts timestamptz not null, 
+	place_id bigint not null references t_place(place_id), 
+	price_man integer not null, 
+	price_woman integer not null
+);
+
+--t_memberテーブルの作るクイリ
+CREATE TABLE t_member(
+	member_id bigserial PRIMARY KEY,
+	member_cd text not null,
+	gender_kbn text not null,
+	family_kj text not null,
+	first_kj text not null,
+	family_kn text not null,
+	first_kn text not null,
+	birthday_ts timestamptz not null
+);
+
+--t_party_memberテーブルの作るクイリ
+CREATE TABLE t_party_member(
+	party_member_id bigserial PRIMARY KEY, 
+	party_id bigint not null references t_party(party_id),
+	member_id bigint not null references t_member(member_id)
+); 
